361f20dd81d7a52dda003a1e29b4ce180fb895d2
Michael <michi.rauer@googlemail>
2018-12-02 20:29:21 +0100

    Added library setup script

---
+++ setup_Rlibs.R
@@ -0,0 +1,24 @@
+packages = c('DT',
+             'data.table',
+             'dplyr',
+             'readr',
+             'ggplot2',
+             'rlang',
+             'crosstalk',
+             'htmlwidget',
+             'tidyr',
+             'tidyselect',
+             'tidyverse')
+
+dir.create("./")
+
+b = !(packages %in% rownames(installed.packages(lib.loc = './Rlib')))
+
+if(all(!b))
+  stop('All packages installed already. Exit')
+
+
+for(p in packages[b]){
+  cat(">> Installing",p,' to ./Rlib\n')
+  install.packages(p, lib = './Rlib/')
+}
