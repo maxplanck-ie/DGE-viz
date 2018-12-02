6b194731e6b6d01b19b0a4360e7997ef732682c2
Michael <michi.rauer@googlemail>
2018-12-02 20:48:38 +0100

    Removed test functions

--- helpers/helpers.R
+++ helpers/helpers.R
@@ -3,15 +3,6 @@ parseData <- function(data.file){
   return(as.data.frame(fread(data.file)))
 }
 
-loadSimulatedData <- function(){
-  source('./helpers/simulator.R')
-  deseq2.tab = produceData()
-}
-
-loadData <- function(tab.file = NULL){
-  if(is.null(tab.file)){
-    return(loadSimulatedData())
-  } else {
-    return(parseData(tab.file))
-  }
+loadData <- function(tab.file){
+  return(parseData(tab.file))
 }
