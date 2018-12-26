b8c5e6cba308a1e015a62083ae4f7ffeadede006
Michael <michi.rauer@googlemail>
2018-12-26 17:01:24 +0100

    Added table format defintions

--- helpers/helpers.R
+++ helpers/helpers.R
@@ -15,3 +15,15 @@ loadData <- function(tab.file = NULL){
     return(parseData(tab.file))
   }
 }
+
+# tablecols.required = c('log2FoldChange','baseMean','pvalue','padj')
+
+format.deseq2 <- function(){
+  # DESeq2 table - baseMean, log2FC, pvalue,padj
+  return(c(3,2,6,7))
+}
+
+format.edger <- function(){
+  # edger table - baseMean, log2FC, pvalue,padj
+  return(c(3,2,5,6))
+}
