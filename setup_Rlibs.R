b13ef050ce452f3ca1f9bdb724bbb588f2f0cd92
Michael Rauer <rauer@ie-freiburg.mpg.de>
2019-12-04 15:51:53 +0100

    Update libraries

--- setup_Rlibs.R
+++ setup_Rlibs.R
@@ -8,7 +8,7 @@ cran.packages = c('DT',
              'htmlwidgets',
              'tidyr',
              'tidyselect',
-             'tidyverse')
+             'janitor')
 
 dir.create("./", showWarnings = TRUE)
 
