6d468e656be8774db7d24acc184a9eadb5a48efa
mirax87 <michi.rauer@googlemail.com>
2020-10-28 11:53:58 +0100

    Running server instance

--- app.R
+++ app.R
@@ -1,11 +1,20 @@
 library(shiny)
 
-rversion = gsub('R version (.*) (.*)','\\1',sessionInfo()$R.version$version.string)
-lib.dir = paste0('Rlib_',rversion)
-library(data.table, lib.loc = lib.dir)
-library(dplyr, lib.loc = lib.dir)
+# .libPaths('/rstudio/rauer/Rlib_DGE-viz_3.6.0')
+
+# sink('/data/processing4/stdout')
+# print(date())
+# print(.libPaths())
+# print(sessionInfo())
+# sink()
+
+lib.dir = '/rstudio/rauer/Rlib_DGE-viz_3.6.0'
 library(plotly, lib.loc = lib.dir)
+library(data.table, lib.loc = lib.dir)
+# library(dplyr, lib.loc = lib.dir)
 library(DT, lib.loc = lib.dir)
+library(ggplot2, lib.loc = lib.dir)
+
 
 tab.colnames = NULL
 gene_id = NULL
@@ -15,25 +24,25 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       fileInput('inputFile','DGE table'),
-      selectInput('inputFormat', label = 'Table format', choices = c('DEseq2','edgeR','others'), multiple = FALSE),
+      selectInput('inputFormat', label = 'Table format', choices = c('DESeq2','edgeR'), selected = 'DESeq2', multiple = FALSE),
       tabsetPanel(
         tabPanel("Parameter",
                  sliderInput('padj.thrs', 'Threshold for padj', value = 0.05, min = 0, max = .1, step = 0.01),
                  sliderInput('logfc.thrs','Threshold for log2 fold-change', value =c(-1,1),min=-8,max=8,step=1/4),
-                 sliderInput('expr.thrs','Thresholds for expression', value=c(-Inf, Inf),min=0,max=log2(2**15),step=1/10,round=TRUE)
+                 sliderInput('expr.thrs','Thresholds for expression', value=c(-Inf, Inf),min=0, max=15,step=1/10,round=TRUE)
                  # fileInput('annotationFile','Annotation GTF'),
-        ),
-        tabPanel("Columns",
-                 selectInput('expr_col', label = 'baseMean', choices = tab.colnames),
-                 checkboxInput('logTransform.expr','do log10', value = TRUE),
-                 selectInput('lfc_col', label = 'log2FoldChange', choices = tab.colnames),
-                 checkboxInput('logTransform.foldchange','do log10', value = FALSE),
-                 selectInput('pvalue_col', label = 'pvalue', choices = tab.colnames),
-                 checkboxInput('logTransform.pvalue','do -log10', value = TRUE),
-                 selectInput('padj_col', label = 'padj', choices = tab.colnames),
-                 checkboxInput('logTransform.padj','do -log10', value = TRUE),
-                 actionButton('update_columns',label = "Update")
         )
+        # tabPanel("Columns",
+        #          selectInput('expr_col', label = 'baseMean', choices = tab.colnames),
+        #          checkboxInput('logTransform.expr','do log10', value = TRUE),
+        #          selectInput('lfc_col', label = 'log2FoldChange', choices = tab.colnames),
+        #          checkboxInput('logTransform.foldchange','do log10', value = FALSE),
+        #          selectInput('pvalue_col', label = 'pvalue', choices = tab.colnames),
+        #          checkboxInput('logTransform.pvalue','do -log10', value = TRUE),
+        #          selectInput('padj_col', label = 'padj', choices = tab.colnames),
+        #          checkboxInput('logTransform.padj','do -log10', value = TRUE),
+        #          actionButton('update_columns',label = "Update")
+        # )
       ),
       verbatimTextOutput('errorOut', placeholder = TRUE)
     ),
@@ -58,33 +67,80 @@ ui <- fluidPage(
 
 server <- function(input, output, session, ...) {
   
-  data_raw = eventReactive(input$inputFile, { 
+  data_raw = eventReactive(input$inputFile, {
+    print(">>> Loading table")
     tab = fread(input$inputFile$datapath)
-    if(colnames(tab)[1] == 'V1')
-      colnames(tab)[1] = 'gene_id'
-    
-    
-    # tab = clean_names(tab)
-    updateSelectInput(session, inputId = 'expr_col', choices = colnames(tab))
-    updateSelectInput(session, inputId = 'lfc_col', choices = colnames(tab))
-    updateSelectInput(session, inputId = 'pvalue_col', choices = colnames(tab))
-    updateSelectInput(session, inputId = 'padj_col', choices = colnames(tab))
-    
-    updateSliderInput(session, inputId = 'expr.thrs',
-                      max = ceiling(max(log2(tab$baseMean))),
-                      value = c(0,ceiling(max(log2(tab$baseMean)))))
-    updateSelectInput(session, inputId = 'genes', choices = setNames(tab$gene_id, nm = tab$symbol))
-    
     return(tab)
   })
   
-  data_sliders = reactive({
-    if(is.null(input$genes))
-      return(data_raw() %>% mutate(selected = padj < input$padj.thrs & 
-                              (log2FoldChange < input$logfc.thrs[1] | log2FoldChange > input$logfc.thrs[2]) &
-                              (input$expr.thrs[1] <= log2(baseMean) & log2(baseMean) <= input$expr.thrs[2])))
+  # data_raw = reactive({
+  #   tab = fread("/data/iovino/group/snakequest_result/FC_exp9_4_1481_RNAseq_DEseq2_st2_st5_Ac_CG_qnVJmJuH_mRNA-seq/analysis/DESeq2_qnVJmJuH_FC_exp9_4_1481_RNAseq_DEseq2_st2_st5_Ac_CG_st5_HDAC3_sampleSheet/DEseq_basic_DEresults.tsv")
+  #   return(tab)
+  # })
+  
+  data_parsed = reactive({
+    tab = data_raw()
+    print(">>> Parsing table")
+    colnames(tab)[1] = 'feature_id'
+    if(any(c('external_gene_name', 'symbol') %in% colnames(tab))){
+      colnames(tab)[colnames(tab) %in% 'external_gene_name'] = 'symbol'
+    } else {
+      tab$symbol = tab$feature_id
+    }
+    cat(">>> Table format:",input$inputFormat,'\n')
+    if(!any(c('DESeq2','edgeR') %in% input$inputFormat)) return(NULL)
     
-    return(data_raw() %>% mutate(selected = gene_id %in% input$genes | symbol %in% input$genes))
+    
+    if(input$inputFormat == 'DESeq2'){
+      tab.out = data.frame(gene_id = tab$feature_id,
+                           baseMean = tab$baseMean, log2FoldChange = tab$log2FoldChange, 
+                           pvalue = tab$pvalue, padj = tab$padj,
+                           symbol = tab$symbol)
+    }
+    
+    if(input$inputFormat == 'edgeR'){
+      tab.out = data.frame(gene_id = tab$feature_id,
+                           baseMean = 2**tab$logCPM, log2FoldChange = tab$logFC, 
+                           pvalue = tab$PValue, padj = tab$FDR,
+                           symbol = tab$symbol)
+    }
+    
+    print('>>> Updating sliders')
+    
+    updateSliderInput(session, inputId = 'expr.thrs',
+                      max = ceiling(max(log2(tab.out$baseMean))),
+                      value = c(0,ceiling(max(log2(tab.out$baseMean)))))
+    
+    lfc.max = ceiling(max(abs(tab.out$log2FoldChange), na.rm = TRUE))
+    cat ('>>> lfc slider max:',lfc.max,'\n')
+    updateSliderInput(session, inputId = 'logfc.thrs',
+                      min = -lfc.max,
+                      max = lfc.max)
+    
+    updateSelectInput(session, inputId = 'genes', choices = setNames(tab.out$gene_id, nm = tab.out$symbol))
+    print('>>> Parsing done')
+    
+    return(tab.out)
+  })
+  
+  data_sliders = reactive({
+    tab <- data_parsed()
+    print(">>> Parsing selection")
+    if(is.null(input$genes)){
+      print(">>> Sliders selection") 
+      tab0 <- tab %>% mutate(selected = padj < input$padj.thrs & 
+                       (log2FoldChange < input$logfc.thrs[1] | log2FoldChange > input$logfc.thrs[2]) &
+                       (input$expr.thrs[1] <= log2(baseMean) & log2(baseMean) <= input$expr.thrs[2]))
+    } else { 
+      print(">>> feature_id selection") 
+      tab0 <- tab %>% mutate(selected = (gene_id %in% input$genes) | (symbol %in% input$genes))
+    }
+    
+    print('>>> Content <selected>')
+    print(table(tab0$selected, useNA = 'always'))
+    
+    tab0 <- tab0 %>% mutate(selected = ifelse(is.na(selected), FALSE, selected))
+    return(tab0)
   })
   
   output$ma <- renderPlotly({
@@ -100,8 +156,8 @@ server <- function(input, output, session, ...) {
     
     height <- session$clientData$output_p_height
     width <- session$clientData$output_p_width
-    
     toWebGL(ggplotly(p1, height = height, width = width, tooltip = c('text','x','y')))
+    
   })
   
   output$volcano <- renderPlotly({
@@ -146,12 +202,17 @@ server <- function(input, output, session, ...) {
   
   output$outtab <- renderDataTable({
     d <- event_data("plotly_selected")
-    if (is.null(d))
-      tab0 = data_sliders() %>% select(-selected)
-    else 
-      tab0 = data_raw() %>% filter(gene_id %in% d$key)
+    tab = data_sliders() 
+    if (is.null(d)){
+      print(">>> outtab: Slider select")
+      tab0 = tab %>% filter(selected) %>% select(-selected)
+    } else {
+      print(">>> outtab: Key select")
+      tab0 = tab %>% filter(gene_id %in% d$key)
+    }
+    
     formatRound(
-      DT::datatable(tab0 ,
+      DT::datatable(tab0,
                     rownames = FALSE,
                     extensions = 'Buttons', options = list(
                       dom = 'Bfrtip',
@@ -162,16 +223,14 @@ server <- function(input, output, session, ...) {
                                        text = 'Download'
                                      ))
                     )),
-      which(sapply(data_raw(), is.numeric)), digits = 4)
-  }, server = FALSE)
+      which(sapply(data_parsed(), is.numeric)), digits = 4)
+  }) #, server = FALSE)
   
   
   output$preview <- renderTable({
-    print(data_raw())
     y = data_raw()[1:5,] %>% as.data.frame
     y = y[,setdiff(colnames(y),'id')]
     y.num = sapply(y, is.numeric)
-    print(y.num)
     y[y.num] = sapply(y[y.num], round, 4)
     return(y)
   })
