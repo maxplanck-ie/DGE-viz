e0b0864226c42e7bf7792384cfe1c93582c80abb
mirax87 <michi.rauer@googlemail.com>
2020-11-04 20:34:36 +0100

    Added stats and sessionInfo

--- app.R
+++ app.R
@@ -1,17 +1,8 @@
 library(shiny)
 
-# .libPaths('/rstudio/rauer/Rlib_DGE-viz_3.6.0')
-
-# sink('/data/processing4/stdout')
-# print(date())
-# print(.libPaths())
-# print(sessionInfo())
-# sink()
-
-lib.dir = '/rstudio/rauer/Rlib_DGE-viz_3.6.0'
+lib.dir = NULL
 library(plotly, lib.loc = lib.dir)
 library(data.table, lib.loc = lib.dir)
-# library(dplyr, lib.loc = lib.dir)
 library(DT, lib.loc = lib.dir)
 library(ggplot2, lib.loc = lib.dir)
 
@@ -30,19 +21,9 @@ ui <- fluidPage(
                  sliderInput('padj.thrs', 'Threshold for padj', value = 0.05, min = 0, max = .1, step = 0.01),
                  sliderInput('logfc.thrs','Threshold for log2 fold-change', value =c(-1,1),min=-8,max=8,step=1/4),
                  sliderInput('expr.thrs','Thresholds for expression', value=c(-Inf, Inf),min=0, max=15,step=1/10,round=TRUE)
-                 # fileInput('annotationFile','Annotation GTF'),
-        )
-        # tabPanel("Columns",
-        #          selectInput('expr_col', label = 'baseMean', choices = tab.colnames),
-        #          checkboxInput('logTransform.expr','do log10', value = TRUE),
-        #          selectInput('lfc_col', label = 'log2FoldChange', choices = tab.colnames),
-        #          checkboxInput('logTransform.foldchange','do log10', value = FALSE),
-        #          selectInput('pvalue_col', label = 'pvalue', choices = tab.colnames),
-        #          checkboxInput('logTransform.pvalue','do -log10', value = TRUE),
-        #          selectInput('padj_col', label = 'padj', choices = tab.colnames),
-        #          checkboxInput('logTransform.padj','do -log10', value = TRUE),
-        #          actionButton('update_columns',label = "Update")
-        # )
+        ),
+        tabPanel("Statistics",
+                 tableOutput('statsOut'))
       ),
       verbatimTextOutput('errorOut', placeholder = TRUE)
     ),
@@ -59,24 +40,19 @@ ui <- fluidPage(
                  plotlyOutput(outputId = "ma"),
                  plotlyOutput(outputId = "volcano")),
         tabPanel('Table - selected data', DT::dataTableOutput('outtab')),
-        tabPanel("Table - preview input", tableOutput('preview'))
-        # ,
-        # tabPanel("Sessioninfo", verbatimTextOutput("sessionInfo"))
+        tabPanel("Table - preview input", tableOutput('preview')),
+        tabPanel("Sessioninfo", verbatimTextOutput("sessionInfo"))
       )))
 )
 
 server <- function(input, output, session, ...) {
   
   data_raw = eventReactive(input$inputFile, {
+    library(dplyr)
     print(">>> Loading table")
     tab = fread(input$inputFile$datapath)
     return(tab)
   })
-  
-  # data_raw = reactive({
-  #   tab = fread("/data/iovino/group/snakequest_result/FC_exp9_4_1481_RNAseq_DEseq2_st2_st5_Ac_CG_qnVJmJuH_mRNA-seq/analysis/DESeq2_qnVJmJuH_FC_exp9_4_1481_RNAseq_DEseq2_st2_st5_Ac_CG_st5_HDAC3_sampleSheet/DEseq_basic_DEresults.tsv")
-  #   return(tab)
-  # })
   
   data_parsed = reactive({
     tab = data_raw()
@@ -126,26 +102,27 @@ server <- function(input, output, session, ...) {
   data_sliders = reactive({
     tab <- data_parsed()
     print(">>> Parsing selection")
-    if(is.null(input$genes)){
-      print(">>> Sliders selection") 
-      tab0 <- tab %>% mutate(selected = padj < input$padj.thrs & 
-                       (log2FoldChange < input$logfc.thrs[1] | log2FoldChange > input$logfc.thrs[2]) &
-                       (input$expr.thrs[1] <= log2(baseMean) & log2(baseMean) <= input$expr.thrs[2]))
-    } else { 
-      print(">>> feature_id selection") 
-      tab0 <- tab %>% mutate(selected = (gene_id %in% input$genes) | (symbol %in% input$genes))
-    }
-    
-    print('>>> Content <selected>')
-    print(table(tab0$selected, useNA = 'always'))
+    tab0 <- tab %>% mutate(selected = padj < input$padj.thrs & 
+                     (log2FoldChange < input$logfc.thrs[1] | log2FoldChange > input$logfc.thrs[2]) &
+                     (input$expr.thrs[1] <= log2(baseMean) & log2(baseMean) <= input$expr.thrs[2]))
     
     tab0 <- tab0 %>% mutate(selected = ifelse(is.na(selected), FALSE, selected))
     return(tab0)
   })
   
+  data_select <- reactive({
+    if(is.null(input$genes)){
+      print(">>> Sliders selection") 
+      tab0 = data_sliders() 
+    } else {
+      print(">>> feature_id selection") 
+      tab0 <- tab %>% mutate(selected = (gene_id %in% input$genes) | (symbol %in% input$genes))
+    }    
+  })
+  
   output$ma <- renderPlotly({
     
-    p1 <- ggplot(data = data_sliders(),  aes(log2(baseMean), log2FoldChange)) + 
+    p1 <- ggplot(data = data_select(),  aes(log2(baseMean), log2FoldChange)) + 
       geom_point(aes(color = selected,
                      text = paste0(symbol,' (',gene_id,')'),
                      key = gene_id), show.legend = FALSE) + 
@@ -161,13 +138,14 @@ server <- function(input, output, session, ...) {
   })
   
   output$volcano <- renderPlotly({
-    
-    p2 <- ggplot(data = data_sliders(), aes(log2FoldChange, -log10(pvalue))) +
+    tab0 = data_select() 
+    pval.cutoff = -log10(max(subset(tab0, padj < input$padj.thrs)$pvalue))
+    p2 <- ggplot(data = data_select(), aes(log2FoldChange, -log10(pvalue))) +
       geom_point(aes(color = selected, 
                      text = paste0(symbol,' (',gene_id,')'),
                      key = gene_id), show.legend = FALSE) + 
       scale_color_manual(values = c('TRUE' = 'blue','FALSE'='grey')) + 
-      geom_hline(yintercept = input$padj.thrs, color = 'darkgrey', lty = 2) + 
+      geom_hline(yintercept = pval.cutoff, color = 'darkgrey', lty = 2) + 
       geom_vline(xintercept = input$logfc.thrs, color = 'darkgrey', lty = 2) +
       theme_light()
     
@@ -176,29 +154,6 @@ server <- function(input, output, session, ...) {
     
     toWebGL(ggplotly(p2, height = height, width = width, tooltip = c('text','x','y')))
   })  
-  
-  # output$p <- renderPlotly({
-  #   # req(input$genes)
-  #   # if (identical(input$genes, "")) return(NULL)
-  #   
-  #   p1 <- ggplot(data = tab0, aes(log2(baseMean), log2FoldChange)) + 
-  #     geom_point(aes(color = external_gene_name %in% input$genes, 
-  #                    text = paste0(external_gene_name,' (',gene_id,')'),
-  #                    key = gene_id)) + 
-  #     scale_color_manual(values = c('TRUE' = 'blue','FALSE'='grey'))
-  #   p2 <- ggplot(data = tab0, aes(log2FoldChange, -log10(pvalue))) +
-  #     geom_point(aes(color = external_gene_name %in% input$genes, 
-  #                    text = paste0(external_gene_name,' (',gene_id,')'),
-  #                    key = gene_id)) + 
-  #     scale_color_manual(values = c('TRUE' = 'blue','FALSE'='grey'))
-  #   height <- session$clientData$output_p_height
-  #   width <- session$clientData$output_p_width
-  #   
-  #   toWebGL(subplot(which_layout = 'merge', 
-  #                   ggplotly(p1, height = height, width = width, tooltip = c('text','x','y')),
-  #                   ggplotly(p2, height = height, width = width, tooltip = c('text','x','y')),
-  #           nrows = 2))
-  # })
   
   output$outtab <- renderDataTable({
     d <- event_data("plotly_selected")
@@ -234,6 +189,19 @@ server <- function(input, output, session, ...) {
     y[y.num] = sapply(y[y.num], round, 4)
     return(y)
   })
+  
+  output$statsOut <- renderTable({
+    tab0 = data_sliders()
+    tab1 = tab0 %>% filter(selected)
+    data.frame(group = c('up','down','total'), 
+               count = c(sum(tab1$log2FoldChange < 0),
+                         sum(tab1$log2FoldChange > 0),
+                         nrow(tab1))) 
+  }, colnames = FALSE)
+  
+  output$sessionInfo <- renderText({
+    paste(capture.output(sessionInfo()),collapse = "\n")
+    })
 }
 
 shinyApp(ui, server)
