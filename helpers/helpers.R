e149c92cdd32be486e978b46371beef2e8bce44a
mirax87 <michi.rauer@googlemail.com>
2021-01-04 12:36:14 +0100

    small refactoring

--- helpers/helpers.R
+++ helpers/helpers.R
@@ -1,4 +1,5 @@
 parse_yaml <- function(config_file){
   t0 = read.table(config_file, sep = ':', header = FALSE, stringsAsFactors = FALSE)
-  setNames(gsub(' +','',t0[,2]), nm = t0[,1])
+  v0 = setNames(gsub(' +','',t0[,2]), nm = t0[,1])
+  v0
 }
